14881e34e98faba20e6cd3bfabbc0715d3010bcd
Apple <apple@AppledeiMac.local>
2026-04-22 19:44:04 +0800

    chore: 清理git跟踪，移除所有数据文件和废弃调试文件，更新.gitignore (v4.22.020)

--- _lines.py
+++
@@ -1,6 +0,0 @@
-with open(r'c:\Users\Administrator\Desktop\线索\线索看板.html','r',encoding='utf-8') as f:
-    lines = f.readlines()
-print('总行数:', len(lines))
-print('最后30行:')
-for i,l in enumerate(lines[-30:], len(lines)-29):
-    print(i, repr(l[:120]))
