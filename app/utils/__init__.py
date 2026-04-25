789d65a31fd9558a2b7a33a5e5c22ceb9ccc2823
孙骥 <sunji@sunbeibei.com>
2026-04-25 22:20:29 +0800

    chore: 删除重构前的Python Flask遗留文件，清理.gitignore和package.json

--- app/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .helpers import _clean_val, _html_escape
-
-__all__ = ['_clean_val', '_html_escape']
--- sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /www/xiansuo-kanban
-git pull
-pkill -f "python3 server.py" || true
-sleep 2
-XIANSUO_BASE_DIR=/www/xiansuo-kanban nohup python3 server.py > server.log 2>&1 &
